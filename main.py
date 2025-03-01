814c653e1a83c3a8421922459b1a2be330f87807
Teenapat Rattanawong <teenapat.rat@gmail.com>
2025-03-01 22:54:03 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from pythainlp.tag import pos_tag
+
+app = FastAPI()
+
+@app.get("/is_noun/{word}")
+async def is_noun(word: str):
+    tags = pos_tag([word], corpus="orchid")
+    for w, tag in tags:
+        if tag == "NCMN":  # NCMN = คำนามใน Thai POS Tagging
+            return {"word": word, "is_noun": True}
+    return {"word": word, "is_noun": False}
